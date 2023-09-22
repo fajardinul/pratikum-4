f3e985b16ba2f7ab75537f02518b879777e1b245
fajardinul <143831361+fajardinul@users.noreply.github.com>
2023-09-22 21:41:17 +0700

    Add files via upload

---
+++ lnk.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+void main() {
+runApp(MainApp());
+}
+class MainApp extends StatefulWidget {
+@override
+_MainAppState createState() => _MainAppState();
+}
+class _MainAppState extends State<MainApp> {
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+home: Scaffold(
+appBar: AppBar(
+title: Text("InkWell FAJAR 4312111004"),
+),
+body: Center(
+child: Ink(
+decoration: BoxDecoration(
+color: Colors.black,
+borderRadius: BorderRadius.circular(24),
+),
+child: InkWell(
+borderRadius: BorderRadius.circular(24),
+onTap: () {
+// Do something when InkWell is tapped
+setState(() {
+// Your code here
+});
+},
+child: Container(
+width: 100.0,
+height: 100.0,
+alignment: Alignment.center,
+child: Text(
+'InkWell',
+style: TextStyle(color: Colors.white, fontSize: 24),
+),
+),
+),
+),
+),
+),
+);
+}
+}
